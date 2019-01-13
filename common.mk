8f0b58d705e745a5d4eb01d98d0e13ad0044193a
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    exynos9610: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df

--- common.mk
+++ common.mk
@@ -314,6 +314,10 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal/exynos-thermal-kane-retin.conf:$(TARGET_COPY_OUT_VENDOR)/exynos-thermal-kane-retin.conf \
     $(COMMON_PATH)/configs/thermal/exynos-thermal-troika.conf:$(TARGET_COPY_OUT_VENDOR)/exynos-thermal-troika.conf
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.exynos9610
