4597e8584919d4e1ff028d99ac5fee7d2a7f97af
Tao Bao <tbao@google.com>
2019-05-06 22:42:02 -0700

    exynos9610-common: AB_OTA_UPDATER is BoardConfig variable
    Bug: 130433003
    Test: TreeHugger
    Change-Id: I70c7a884df45781e8b14339edd02985ba3de02fd

--- common.mk
+++ common.mk
@@ -29,15 +29,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(COMMON_PATH)/overlay/lineage-sdk
 
 # A/B
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    vbmeta \
-    vendor
-
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
