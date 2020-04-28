b19bb8fbe3fcc32c72656261b34c03d272728ce3
Jan Altensen <info@stricted.net>
2020-04-28 17:26:04 +0200

    exynos9610: import HotwordEnrollement
    Change-Id: I1e196c46b18f2c55df428906be4cc085d0aa0f70

--- common.mk
+++ common.mk
@@ -57,3 +57,7 @@ PRODUCT_AAPT_CONFIG := xlarge
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 # A list of dpis to select prebuilt apk, in precedence order.
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
+
+# HotwordEnrollement
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
