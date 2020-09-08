1dbca3b6447b0d6d1d0bb1182765896d52bd68ef
Jan Altensen <info@stricted.net>
2020-09-08 20:09:26 +0200

    exynos9610: copy libGLES_mali.so to vulkan.exynos9610.so
     * symlinks arent working for some reason
    
    Change-Id: Ic0b8f5158577447bdfd23d3fa7499fc70bb73f33

--- Android.mk
+++ Android.mk
@@ -22,7 +22,7 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-EGL_LIBS := libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 hw/vulkan.exynos9610.so
+EGL_LIBS := libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1
 
 EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
 $(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
@@ -38,6 +38,22 @@ $(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@rm -rf $@
 	$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
+VULKAN_LIBS := vulkan.exynos9610.so
+
+VULKAN_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/hw/,$(VULKAN_LIBS))
+$(VULKAN_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Copy Vulkan lib: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) cp $(TARGET_OUT_VENDOR)/lib/egl/libGLES_mali.so $@
+
+VULKAN_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/hw/,$(VULKAN_LIBS))
+$(VULKAN_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Copy Vulkan lib: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) cp $(TARGET_OUT_VENDOR)/lib64/egl/libGLES_mali.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS) $(VULKAN_32_SYMLINKS) $(VULKAN_64_SYMLINKS)
 
 endif
