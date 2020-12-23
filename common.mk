04c43b2d9dd19293f50bb630e35aa42dc1ef4cef
Jan Altensen <info@stricted.net>
2020-12-23 16:40:54 +0100

    exynos9610: use MotoActions from hardware/motorola
    Change-Id: Ic63fdbbe9f3e908b515a1a39da1c9de53b9e2ed6

--- common.mk
+++ common.mk
@@ -189,9 +189,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.exynos9610
 
-# LineageActions
+# MotoActions
 PRODUCT_PACKAGES += \
-    LineageActions
+    MotoActions
 
 # Media
 PRODUCT_COPY_FILES += \
