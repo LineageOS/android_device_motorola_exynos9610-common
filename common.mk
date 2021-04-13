24d01d84e1d76808a0fcffb7a1292202b5f11c6e
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    exynos9610: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- common.mk
+++ common.mk
@@ -65,6 +65,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
+    android.hardware.bluetooth.audio@2.1-impl \
     libbt-vendor
 
 PRODUCT_COPY_FILES += \
