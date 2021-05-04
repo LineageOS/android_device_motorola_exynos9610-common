df674329622e767938dd6754caa7ca677e975b71
Jan Altensen <info@stricted.net>
2021-05-04 02:55:46 +0200

    exynos9610: build lights hal from hardware/samsung
    Change-Id: I81d3bb350319c5449e1a4039da7558ad4c6b0772

--- common.mk
+++ common.mk
@@ -191,7 +191,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.exynos9610
+    android.hardware.light@2.0-service.samsung
 
 # MotoActions
 PRODUCT_PACKAGES += \
