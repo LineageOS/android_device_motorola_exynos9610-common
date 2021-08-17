538a9138e222ce1b93df0d88e6ad61d22c7f767c
Jan Altensen <info@stricted.net>
2021-08-17 13:20:34 +0200

    exynos9610: update blobs to RSB31.Q1-48-36-11
    Change-Id: I6a2cbe2dacad5ef3e02f1c8c80e9ca234d9f02e9

--- common.mk
+++ common.mk
@@ -258,10 +258,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
     android.hardware.power@1.0-service
 
-# Protobuf
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
-
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
