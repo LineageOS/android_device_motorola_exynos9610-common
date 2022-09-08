2c95c27d9b2c5d1b88cba1e40ae4d67bb125fc48
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-08 23:40:58 +0200

    exynos9610: Retrofit dynamic partitions
    * Merge system and vendor to make use of 500mb unused on /vendor.
    
    Change-Id: Ib8c8ea5ea5d93071097e79ae60fc98a72096aa1a

--- common.mk
+++ common.mk
@@ -143,6 +143,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
     libprotobuf-cpp-lite-3.9.1-vendorcompat
 
+# Dynamic Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1 \
