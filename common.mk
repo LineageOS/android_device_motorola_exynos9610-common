dc3d4774bc2e721f69ff102c75934c208c13f9b3
Georg Veichtlbauer <georg@vware.at>
2023-03-09 15:34:20 +0100

    exynos9610: Remove unneeded NFC packages
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- common.mk
+++ common.mk
@@ -234,8 +234,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.samsung \
     com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
