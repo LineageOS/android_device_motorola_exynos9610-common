a9f4e1d73aba44259119985a599078cab2d67885
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    exynos9610: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- common.mk
+++ common.mk
@@ -115,7 +115,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # FastCharge
 PRODUCT_PACKAGES += \
