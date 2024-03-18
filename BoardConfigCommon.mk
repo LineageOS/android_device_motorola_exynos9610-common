69cabfc502a8e4867364de45d54084d393e1f4e4
Bruno Martins <bgcngm@gmail.com>
2024-03-18 13:14:18 +0000

    exynos9610: Declare vulkan lib as a symlink during extraction
    Change-Id: I2678e73e957081db7d988da589892b482985778e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,7 +22,6 @@ include vendor/motorola/exynos9610-common/BoardConfigVendor.mk
 ## BUILD_BROKEN_*
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true
 
 ## A/B
 AB_OTA_UPDATER := true
