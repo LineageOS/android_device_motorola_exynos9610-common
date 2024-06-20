8cfb9dc7b1c97bf322ddb255cfba24b0e594c9b3
Nolen Johnson <johnsonnolen@gmail.com>
2024-06-20 01:00:00 -0400

    Revert "exynos9610: Enforce vintf kernel requirements"
    This reverts commit d207355d893a6c3d47bed644f7a0fd824ea39ef2.
    
    Reason: After moving back to stock-based kernel, subversion is too old.
    Change-Id: I61840fc7ef1400e7324034175a734bb8db82b03b

--- common.mk
+++ common.mk
@@ -439,9 +439,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service.exynos9610
 
-# VINTF
-PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
-
 # VNDK
 PRODUCT_PACKAGES += \
     libutils-v32
