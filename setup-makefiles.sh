28c3974cc1f6e29b27090ffed749ff13377739c8
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-10 17:01:17 -0500

    exynos9610-common: Enable ELF checks
    Change-Id: If3a98f51add72942d2d566aa69689739f7e9c67a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -14,14 +14,20 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
-export TARGET_ENABLE_CHECKELF=false
-
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
+
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/motorola/exynos9610-common",
+		"hardware/samsung_slsi-linaro/exynos",
+		"hardware/samsung_slsi-linaro/graphics",
+EOF
+}
 
 # Initialize the helper for common
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
