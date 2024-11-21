895a8cca33d418e5dfb0da66b41dfc639f0c8e8d
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-21 14:05:34 -0500

    exynos9610: Switch to py extract
    Change-Id: I3d4bea635c390d8e682f878f86dee66f362ab52e

---
+++ extract-files.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    BlobFixupCtx,
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixup_remove_arch_suffix,
+    lib_fixups,
+    lib_fixups_user_type,
+    lib_fixup_vendorcompat,
+    libs_clang_rt_ubsan,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
+
+namespace_imports = [
+    'device/motorola/exynos9610-common',
+    'hardware/samsung_slsi-linaro/graphics',
+]
+
+libs_remove = (
+     'libandroidicu',
+     'libcsc',
+     'libexynosutils',
+     'libexynosv4l2',
+     'libGrallocWrapper',
+)
+
+lib_fixups: lib_fixups_user_type = {
+    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    libs_remove: lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system/app/ShannonIms/ShannonIms.apk': blob_fixup()
+        .apktool_patch('blob-patches/ShannonIms', '-r'),
+    'system/lib64/libmediaadaptor.so': blob_fixup()
+        .patchelf_version('0_18')
+        .replace_needed('libmedia.so', 'libmedia_ims.so'),
+    'system_ext/app/ShannonDataService/ShannonDataService.apk': blob_fixup()
+        .apktool_patch('blob-patches/ShannonDataService', '-r'),
+    'system_ext/app/ShannonNetworkService/ShannonNetworkService.apk': blob_fixup()
+        .apktool_patch('blob-patches/ShannonNetworkService', '-r'),
+    'system_ext/app/ShannonQualifiedNetworksService/ShannonQualifiedNetworksService': blob_fixup()
+        .apktool_patch('blob-patches/ShannonQualifiedNetworksService', '-r'),
+    ('vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service-rbs', 'vendor/lib64/hw/android.hardware.gnss@1.0-impl.samsung.so', 'vendor/lib64/hw/android.hardware.gnss@1.1-impl.samsung.so', 'vendor/lib64/hw/android.hardware.gnss@2.0-impl.samsung.so', 'vendor/lib64/hw/vendor.samsung.hardware.gnss@1.0-impl.so'): blob_fixup()
+         .patchelf_version('0_18')
+         .remove_needed('libhidltransport.so')
+         .remove_needed('libhwbinder.so'),
+    'vendor/bin/charge_only_mode': blob_fixup()
+        .patchelf_version('0_18')
+        .add_needed('libmemset_shim.so'),
+    'vendor/bin/wfc-pkt-router': blob_fixup()
+        .patchelf_version('0_18')
+        .replace_needed('libnetutils.so', 'libip_checksum_shim.so'),
+    'vendor/etc/permissions/com.motorola.motosignature.xml': blob_fixup()
+        .regex_replace('system', 'vendor'),
+    ('vendor/lib/libhifills.so', 'vendor/lib64/libhifills.so'): blob_fixup()
+        .patchelf_version('0_18')
+        .add_needed('libdemangle.so')
+        .add_needed('libprocessgroup.so'),
+    ('vendor/lib/libwvhidl.so', 'vendor/lib64/libstrongswan.so'): blob_fixup()
+        .patchelf_version('0_18')
+        .add_needed('libcrypto_shim.so'),
+    ('vendor/lib/sensors.rp.so', 'vendor/lib64/sensors.rp.so'): blob_fixup()
+        .patchelf_version('0_18')
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'exynos9610-common',
+    'motorola',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
