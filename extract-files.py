963676b1a90020fca45ce0465fa9b17a9ae73ee0
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-26 23:29:53 -0500

    exynos9610: ruff check --fix
    Change-Id: I46cf7d87daeac23d311d3e5aabc0442fbae49662

--- extract-files.py
+++ extract-files.py
@@ -4,16 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from extract_utils.file import File
 from extract_utils.fixups_blob import (
-    BlobFixupCtx,
     blob_fixup,
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
     lib_fixup_remove,
     lib_fixup_remove_arch_suffix,
-    lib_fixups,
     lib_fixups_user_type,
     lib_fixup_vendorcompat,
     libs_clang_rt_ubsan,
@@ -22,12 +19,6 @@ from extract_utils.fixups_lib import (
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
-)
-from extract_utils.tools import (
-    llvm_objdump_path,
-)
-from extract_utils.utils import (
-    run_cmd,
 )
 
 namespace_imports = [
