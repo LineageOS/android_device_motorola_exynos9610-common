b8dbb97a47bfce2c5d02c529f7b1fb027befe105
Stricted <info@stricted.net>
2024-11-28 11:31:03 +0100

    exynos9610: build lineage power-libperfmgr
    * powerhint.json: loosely based on https://github.com/LineageOS/android_device_samsung_universal9611-common/blob/lineage-20/configs/power/powerhint.json @ a6f8e2c5c5f374d02ef4686b52bdd5909cc5f310
    * powerhint.json: values extracted from stock blob
    
    Change-Id: I5ca86520d7d685fc1c36d3eca7cd2b44b38a8d3c

--- common.mk
+++ common.mk
@@ -338,8 +338,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power-service.lineage-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
@@ -369,6 +371,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/samsung \
     hardware/samsung_slsi/libbt
 
