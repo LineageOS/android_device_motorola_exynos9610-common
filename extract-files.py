a7e52960213d44e96d8eecb45369cd513e6197a1
Stricted <info@stricted.net>
2025-01-23 11:54:18 +0100

    exynos9610: build camera hal from source
    turns out the source released by linaro is extremely close to what was used on stock
    main difference i can spot is minor refactoring in the way processing pipelines are
    allocated, for example, the stock hal always created a 3AF pipeline but it was later
    in the code disabled, in the bsp hal they guarded it at compile time, in theory this hal
    should be better with ressource management
    
    what we lose compared to stock:
    - "ExynosCameraMakersNoteLenovo", custom moto exif extension, stock camera hal wont work without
    - factory led calibration
    
    Change-Id: I9f5a713a516eea9a0ef2bacc8a250bf8c3df56d1

--- extract-files.py
+++ extract-files.py
@@ -49,8 +49,6 @@ blob_fixups: blob_fixups_user_type = {
         .remove_needed('libhwbinder.so'),
     'vendor/bin/wfc-pkt-router': blob_fixup()
         .replace_needed('libnetutils.so', 'libip_checksum_shim.so'),
-    'vendor/etc/permissions/com.motorola.motosignature.xml': blob_fixup()
-        .regex_replace('system', 'vendor'),
     'vendor/lib/libaudioproxy.so': blob_fixup()
         .add_needed('libaudioproxy_shim.so'),
     ('vendor/lib/libhifills.so', 'vendor/lib64/libhifills.so'): blob_fixup()
