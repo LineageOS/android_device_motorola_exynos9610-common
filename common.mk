3d7e6757462bb9a73197068453c3d61e9be7352a
Stricted <info@stricted.net>
2025-01-23 11:54:18 +0100

    exynos9610: build camera hal from source
    turns out the source released by linaro is extremely close to what was used on stock
    main difference i can spot is minor refactoring in the way processing pipelines are
    allocated, for example, the stock hal always created a 3AF pipeline but it was later
    in the code disabled, in the bsp hal they guarded it at compile time, in theory this hal
    should be better with ressource management
    
    what we lose compared to stock:
    - "ExynosCameraMakersNoteLenovo", custom moto exif extension, stock camera app wont work without
    - factory led calibration
    
    Change-Id: I9f5a713a516eea9a0ef2bacc8a250bf8c3df56d1

--- common.mk
+++ common.mk
@@ -93,7 +93,13 @@ PRODUCT_PACKAGES += \
     android.frameworks.cameraservice.service@2.1.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    libgiantmscl
+    camera.exynos9610 \
+    libexynoscamera_hifi_plugin \
+    libexynoscamera_hifills_plugin \
+    libexynoscamera_plugin \
+    libexynoscamera_plugin_utils \
+    libexynoscamera_vdis_plugin \
+    libgiantmscl \
 
 # ConfigStore
 PRODUCT_PACKAGES += \
@@ -376,6 +382,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/samsung \
+    hardware/samsung_slsi-linaro/camera/N23 \
     hardware/samsung_slsi-linaro/exynos/c2service \
     hardware/samsung_slsi-linaro/exynos/gralloc/gralloc3 \
     hardware/samsung_slsi-linaro/exynos/tee/kinibi410 \
