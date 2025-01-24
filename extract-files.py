d042fb947fbfaf4956bcf906bf4ab50e6e1e6326
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-24 15:14:59 -0500

    exynos9610: Fix extract spacing
    Change-Id: I53eaab60d0046c05279bfdf01efbf2f7be759079

--- extract-files.py
+++ extract-files.py
@@ -36,7 +36,7 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system/app/ShannonIms/ShannonIms.apk': blob_fixup()
         .apktool_patch('blob-patches/ShannonIms', '-r'),
-   'system/lib64/libmediaadaptor.so': blob_fixup()
+    'system/lib64/libmediaadaptor.so': blob_fixup()
         .add_needed('libmediaadaptor_shim.so'),
     'system_ext/app/ShannonDataService/ShannonDataService.apk': blob_fixup()
         .apktool_patch('blob-patches/ShannonDataService', '-r'),
