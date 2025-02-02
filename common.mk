3f84fb8109db90fb4f2f92467a1ce0ac9270ee5f
Nolen Johnson <johnsonnolen@gmail.com>
2025-02-02 14:19:49 -0500

    exynos9610: Build audio_abox from source
    Change-Id: I119e8385dcf686edc3a87ba505d064bf117c3cbf

--- common.mk
+++ common.mk
@@ -46,7 +46,8 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
-    libgui_vendor
+    libgui_vendor \
+    main_abox
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
