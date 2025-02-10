835bc9969021a2b0788c2986ee22a18add036572
Jan Altensen <info@stricted.net>
2025-02-10 17:49:29 +0100

    exynos9610: switch to common lineage lights hal
    Change-Id: I6ee0b939004a1ff993e92f1f55133e92f48ca89c

--- common.mk
+++ common.mk
@@ -226,7 +226,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.samsung
+    android.hardware.light-service.lineage
 
 # Lineage Health
 PRODUCT_PACKAGES += \
