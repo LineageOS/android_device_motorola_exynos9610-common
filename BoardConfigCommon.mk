9643d287e1b668c30a38d7e86fc4c0f7b47755c6
Michael Bestas <mkbestas@lineageos.org>
2025-02-23 03:00:52 +0200

    exynos9610: Eliminate system.prop
    Change-Id: Ia05f96ee124b6164f6b6d7d9b29622dd9abfbba5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -92,7 +92,7 @@ TARGET_SOC := exynos9610
 ## Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
-TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 ## Recovery
