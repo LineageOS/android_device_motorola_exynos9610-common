090bf26e982996993b53efcf72e29d990ccdc8db
LuK1337 <priv.luk@gmail.com>
2025-07-06 12:54:55 +0200

    exynos9610: apktool_patch() no longer takes custom arguments
    Change-Id: Idf85ae2cd0505c34e55f0e6acec2afc085372b78

--- extract-files.py
+++ extract-files.py
@@ -35,15 +35,15 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     'system/app/ShannonIms/ShannonIms.apk': blob_fixup()
-        .apktool_patch('blob-patches/ShannonIms', '-r'),
+        .apktool_patch('blob-patches/ShannonIms'),
     'system/lib64/libmediaadaptor.so': blob_fixup()
         .add_needed('libnativehelper_shim.so'),
     'system_ext/app/ShannonDataService/ShannonDataService.apk': blob_fixup()
-        .apktool_patch('blob-patches/ShannonDataService', '-r'),
+        .apktool_patch('blob-patches/ShannonDataService'),
     'system_ext/app/ShannonNetworkService/ShannonNetworkService.apk': blob_fixup()
-        .apktool_patch('blob-patches/ShannonNetworkService', '-r'),
+        .apktool_patch('blob-patches/ShannonNetworkService'),
     'system_ext/app/ShannonQualifiedNetworksService/ShannonQualifiedNetworksService.apk': blob_fixup()
-        .apktool_patch('blob-patches/ShannonQualifiedNetworksService', '-r'),
+        .apktool_patch('blob-patches/ShannonQualifiedNetworksService'),
     ('vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service-rbs', 'vendor/lib64/hw/android.hardware.gnss@1.0-impl.samsung.so', 'vendor/lib64/hw/android.hardware.gnss@1.1-impl.samsung.so', 'vendor/lib64/hw/android.hardware.gnss@2.0-impl.samsung.so', 'vendor/lib64/hw/vendor.samsung.hardware.gnss@1.0-impl.so'): blob_fixup()
         .remove_needed('libhidltransport.so')
         .remove_needed('libhwbinder.so'),
