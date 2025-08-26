66f3645062b4d20f1cc0be7aa9907a8bc17845e8
Nolen Johnson <johnsonnolen@gmail.com>
2025-08-26 17:22:54 -0400

    exynos9610: Use AIDL LiveDisplay from hardware/samsung
    Change-Id: I184aaacdec202d5e88c87acd97cd5ae0a45192a8

--- common.mk
+++ common.mk
@@ -240,7 +240,7 @@ $(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 
 # Livedisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.exynos9610
+    vendor.lineage.livedisplay-service.samsung-exynos
 
 # Media
 PRODUCT_PACKAGES += \
