0171d4add72b4058464fad3879cfd45da46319c5
Nolen Johnson <johnsonnolen@gmail.com>
2026-05-20 23:04:56 -0400

    exynos9610: Stop excluding non-existent MotoSignatureApp from dex-opt
    * Was barely needed back when MotoCamera2 was included.
    
    Change-Id: Idf6bc1d968fe68be2fc90ed390e0a7b82dc48673

--- common.mk
+++ common.mk
@@ -131,9 +131,6 @@ $(call soong_config_set_bool,exynos_camera,uses_slsi_vendor_tags,true)
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.1-service
 
-# Dex-pre-opt exclusions
-$(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
-
 # ConfigStore
 PRODUCT_PACKAGES += \
     disable_configstore
