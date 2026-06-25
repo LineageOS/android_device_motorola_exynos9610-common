ccc6b60a7fb793845d6b19bddc8693ec61ec365f
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 19:19:11 -0400

    exynos9610: Stop disabling configstore
    * As of CP2A it's just dead and gone.
    
    Change-Id: I671b668c4bb9a967f779753233c2025a428c9421

--- common.mk
+++ common.mk
@@ -116,14 +116,6 @@ $(call soong_config_set_bool,exynos_camera,uses_sensor_listener,true)
 $(call soong_config_set_bool,exynos_camera,uses_slsi_plugin,true)
 $(call soong_config_set_bool,exynos_camera,uses_slsi_vendor_tags,true)
 
-# ConfigStore
-PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.1-service
-
-# ConfigStore
-PRODUCT_PACKAGES += \
-    disable_configstore
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
