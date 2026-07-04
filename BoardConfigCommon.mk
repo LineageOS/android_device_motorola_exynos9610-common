5c8989496cb6bdbd7e19cd66142b31d49e691d03
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-04 14:13:18 -0400

    exynos9610: sepolicy: Associate coredomain to IMS/RCS apps
    * Sadly triggers neverallows, let it happen.
    
    Change-Id: I2403f50e0121701ae3790124ad6f330b0991f3d1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,6 +135,7 @@ VENDOR_SECURITY_PATCH := 2022-07-01
 
 ## SELinux
 BOARD_SEPOLICY_TEE_FLAVOR := mobicore
+SELINUX_IGNORE_NEVERALLOWS := true
 include device/lineage/sepolicy/exynos/sepolicy.mk
 include device/lineage/sepolicy/libion/sepolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
