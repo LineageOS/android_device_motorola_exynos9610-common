7f621048fbc7f037b96c70a3dfdda743eb9364d3
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-05 18:36:52 -0400

    exynos9610: Move system/product sepolicy -> system_ext
    Change-Id: Id7f76643dbdeadac0ab940af08f58b755ea0ad3f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -140,7 +140,7 @@ include device/lineage/sepolicy/libion/sepolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/samsung_slsi/sepolicy/sepolicy.mk
 
-PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/system_ext/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 ## Verified Boot
