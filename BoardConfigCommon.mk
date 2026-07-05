c0c68b4ecf43c36e9b314cc2068094067728fbd1
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-05 18:37:31 -0400

    exynos9610: sepolicy: Split shannon policy across system_ext/vendor
    Change-Id: Ic76f59157f8ce3000685f88d5ccd099a536e1754

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,6 +114,7 @@ include device/lineage/sepolicy/exynos/sepolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/samsung_slsi/sepolicy/sepolicy.mk
 
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/system_ext/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/system_ext/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
